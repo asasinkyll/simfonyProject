ec9779185370a17cf6db456a4e278d051c0054a5
Assasin <asasin_kyll@yahoo.ro>
2017-08-12 14:08:34 +0300

    Tutorial 2

--- src/AppBundle/Controller/GitHutController.php
+++ src/AppBundle/Controller/GitHutController.php
@@ -21,34 +21,44 @@ class GitHutController extends Controller
      */
     public function githutAction(Request $request)
     {
-        return $this->render('githut/index.html.twig');
-    }
-
-    /**
-     * @Route("/test", name="test")
-     */
-    public function testAction(Request $request)
-    {
-        return $this->render('base.html.twig');
-    }
-
-    /**
-     * @Route("/test/{testnumber}")
-     */
-    public function parameterAction(Request $request, $testnumber)
-    {
-        return $this->render('githut/test.html.twig', [
-            'test' => $testnumber,
-            'pagetitle' => "acesta este titlul pagini"
+        return $this->render('githut/index.html.twig', [
+            'avatar_url'    => "https://avatars2.githubusercontent.com/u/12968163?v=4",
+            'name'          => "Code Review Videos",
+            'login'         => "codereviwsvideos",
+            'details'       =>[
+                'company'   => "Code Review Videos",
+                'location'  => "Preston, Lancs, UK",
+                'joined_on' => "joined on: 2015-06-19"
+            ],
+            'blog'          => "https://codereviewvideos.com/",
+            'social_data'   =>[
+                'Public Repos'  => 39,
+                'Followers'     => 51,
+                'Following'     => 1,
+            ],
         ]);
     }
 
-
-    /**
-     * @Route("/misto/{misto}")
-     */
-    public function mistoAction(Request $request, $misto)
-    {
-        return $this->render('githut/test.html.twig');
-    }
+//    /**
+//     * @Route("/profile", name="profile")
+//     */
+//    public function profileAction(Request $request)
+//    {
+//        return $this->render("githut/profile.html.twig", [
+//            'avatar_url'    => "https://avatars2.githubusercontent.com/u/12968163?v=4",
+//            'name'          => "Code Review Videos",
+//            'login'         => "codereviwsvideos",
+//            'details'       =>[
+//                'company'   => "Code Review Videos",
+//                'location'  => "Preston, Lancs, UK",
+//                'joined_on' => "joined on: 2015-06-19"
+//            ],
+//            'blog'          => "https://codereviewvideos.com/",
+//            'social_data'   =>[
+//                'Public Repos'  => 39,
+//                'Followers'     => 51,
+//                'Following'     => 1,
+//            ],
+//        ]);
+//    }
 }
